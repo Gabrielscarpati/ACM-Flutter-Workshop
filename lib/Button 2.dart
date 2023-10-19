79359611470fe0b8ef126340dad8596686d4abb5
Mihir16x <114958905+Mihir16x@users.noreply.github.com>
2023-10-18 22:24:49 -0400

    Create Button 2.dart

---
+++ lib/Button 2.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const ProfileScreen(),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget{
+  const ProfileScreen ({super.key});
+
+  @override
+  Widget build(BuildContext context){
+
+    String hintName = "Type name here...";
+    String hintEmail = "Type email here...";
+    String hintPhone = "Type phone here...";
+    String hintAddres = "Type address here...";
+    double spacing =12.0;
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+
+
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(12),
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+
+              backgroundColor: Colors.yellow.shade600,
+              padding: const EdgeInsets.all(15),
+
+            ),
+            child: const Text(
+              'Edit Profile',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              )
+            )
+          )
+        ),
+      )
+    );
+  }
+
+}
