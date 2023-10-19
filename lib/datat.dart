a3e71df650e24e8bf0319ab17f8af8a8a57eb3a9
Mihir16x <114958905+Mihir16x@users.noreply.github.com>
2023-10-19 16:15:01 -0400

    Create datat.dart

---
+++ lib/datat.dart
@@ -0,0 +1,29 @@
+void main() {
+
+  // declare an integer
+  int num1 = 2;			 
+	
+  // declare a double value
+  double num2 = 1.5; 
+
+  // print the values
+  print(num1);
+  print(num2);
+  int a1 = 1;
+  double b1 = 2.34; 
+	
+
+  String string = 'ACM''at''USF'; 
+  String str = 'Coding is '; 
+  String str1 = 'Fun'; 
+  print(string);  
+  print(str + str1);
+
+  bool val = (str==str1);
+  print (val); 
+
+  num result = a1 + b1;
+  print(result);
+  print(result.runtimeType);
+  
+}
